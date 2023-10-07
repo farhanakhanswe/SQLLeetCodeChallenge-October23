c5034eef6d200b4b159e958c77b232b8e503c46b
Farhana Khan <134283374+farhanakhanswe@users.noreply.github.com>
2023-10-07 21:48:15 +0600

    Solution for Number-of-Unique-Subjects-Taught-by-Each-Teacher question

---
+++ Easy/Number-of-Unique-Subjects-Taught-by-Each-Teacher.sql
@@ -0,0 +1,7 @@
+# Question Link: https://leetcode.com/problems/number-of-unique-subjects-taught-by-each-teacher/description/
+
+# Solution:
+  
+SELECT `teacher_id`, COUNT(DISTINCT `subject_id`) AS `cnt`
+FROM `Teacher`
+GROUP BY `teacher_id`;
