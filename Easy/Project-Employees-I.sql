eae7fbe6496659ce9b8a4c56ce596f8abb7b05bf
Farhana Khan <134283374+farhanakhanswe@users.noreply.github.com>
2023-10-09 23:49:50 +0600

    Solution for Project-Employees-I question

---
+++ Easy/Project-Employees-I.sql
@@ -0,0 +1,8 @@
+# Question Link: https://leetcode.com/problems/project-employees-i/
+
+# Solution:
+
+SELECT project_id, ROUND(AVG(experience_years), 2) AS average_years
+FROM Project
+JOIN Employee ON Project.employee_id = Employee.employee_id
+GROUP BY project_id;
