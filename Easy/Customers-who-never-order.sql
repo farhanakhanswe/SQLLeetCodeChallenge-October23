75f02ca3ab68dc2f550f5dd13a3cc78d21667c32
Farhana Khan <134283374+farhanakhanswe@users.noreply.github.com>
2023-10-15 01:29:02 +0600

    Solution for Customers-who-never-order.sql

---
+++ Easy/Customers-who-never-order.sql
@@ -0,0 +1,8 @@
+# Question Link: https://leetcode.com/problems/customers-who-never-order/
+
+# Solution:
+
+SELECT customers.name AS 'Customers'
+FROM customers
+LEFT JOIN orders ON customers.id = orders.customerId
+WHERE orders.customerId IS NULL;
