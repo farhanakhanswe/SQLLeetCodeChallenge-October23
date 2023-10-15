d93ddcfd6492bda5f3484c11386d4bcf407d7672
Farhana Khan <134283374+farhanakhanswe@users.noreply.github.com>
2023-10-15 23:16:50 +0600

    Solution for Customer-placing-the-largest-number-of-orders

---
+++ Easy/Customer-placing-the-largest-number-of-orders.sql
@@ -0,0 +1,9 @@
+# Question Link: https://leetcode.com/problems/customer-placing-the-largest-number-of-orders/
+
+# Solution:
+
+SELECT customer_number
+FROM orders
+GROUP BY (customer_number)
+ORDER BY COUNT(order_number) DESC
+LIMIT 1;
