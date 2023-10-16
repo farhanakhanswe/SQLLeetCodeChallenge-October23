e5322fed41ed9e16efb64f64b9b0432931b35303
Farhana Khan <134283374+farhanakhanswe@users.noreply.github.com>
2023-10-16 23:44:03 +0600

    solution for game-play-analysis-i.sql

---
+++ Easy/game-play-analysis-i.sql
@@ -0,0 +1,7 @@
+# Question Link: https://leetcode.com/problems/game-play-analysis-i/submissions/
+
+# Solution:
+
+SELECT player_id, MIN(event_date) AS first_login
+FROM activity
+GROUP BY player_id;
