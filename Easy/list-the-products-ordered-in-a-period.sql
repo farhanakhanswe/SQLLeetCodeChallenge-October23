dad986926b5cd5df4f3ff56daa4e35ea72b969b8
Farhana Khan <134283374+farhanakhanswe@users.noreply.github.com>
2023-10-26 01:06:19 +0600

    solution for list-the-products-ordered-in-a-period.sql

---
+++ Easy/list-the-products-ordered-in-a-period.sql
@@ -0,0 +1,11 @@
+# Question Link: https://leetcode.com/problems/list-the-products-ordered-in-a-period/
+
+# Solution:
+
+SELECT p.product_name, SUM(o.unit) AS "unit"
+FROM products as p
+JOIN orders as o
+ON p.product_id = o.product_id 
+WHERE o.order_date BETWEEN "2020-02-01" AND "2020-02-29"
+GROUP BY o.product_id
+HAVING SUM(o.unit) >= 100; 
