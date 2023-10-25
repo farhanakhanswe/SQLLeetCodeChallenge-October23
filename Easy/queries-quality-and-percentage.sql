80922ed3f6834531fab8ebe00a286f79626df3d4
Farhana Khan <134283374+farhanakhanswe@users.noreply.github.com>
2023-10-25 19:11:50 +0600

    Update queries-quality-and-percentage.sql

--- Easy/queries-quality-and-percentage.sql
+++ Easy/queries-quality-and-percentage.sql
@@ -8,3 +8,12 @@ ROUND(AVG(rating/position),2) AS quality,
 ROUND(SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) / COUNT(query_name) * 100, 2) AS poor_query_percentage
 FROM queries
 GROUP BY query_name;
+
+# O R
+
+SELECT 
+  query_name, 
+  ROUND(AVG(rating/position), 2) AS quality, 
+  ROUND((COUNT(CASE WHEN rating < 3 THEN 1 ELSE NULL END) / COUNT(query_name)) * 100, 2) AS poor_query_percentage
+FROM queries
+GROUP BY query_name;
