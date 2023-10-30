847e5dbc300e3cad577f177ccd86623fb688ddc2
Farhana Khan <134283374+farhanakhanswe@users.noreply.github.com>
2023-10-31 00:39:00 +0600

    solution for group-sold-products-by-the-date.sql

---
+++ Easy/group-sold-products-by-the-date.sql
@@ -0,0 +1,11 @@
+# Question Link: https://leetcode.com/problems/group-sold-products-by-the-date/
+
+# Solution:
+  
+SELECT 
+  sell_date, 
+  COUNT(DISTINCT product) AS "num_sold", 
+  GROUP_CONCAT(DISTINCT product ORDER BY product ASC SEPARATOR ',') AS "products" 
+FROM activities
+GROUP BY sell_date
+ORDER BY sell_date;
