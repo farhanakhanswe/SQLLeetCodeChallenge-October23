bde861f6291130b59ba439c1be19f9d3772036da
farhanakhanswe <farhanakhanswe@gmail.com>
2023-11-01 19:11:44 +0600

    soln for the latest login

--- Easy/the-latest-login-in-2020.sql
+++ Easy/the-latest-login-in-2020.sql
@@ -1,1 +1,8 @@
+# Question Link: https://leetcode.com/problems/the-latest-login-in-2020/
 
+# Solution: 
+
+SELECT user_id, MAX(time_stamp) AS 'last_stamp'
+FROM logins
+WHERE YEAR(time_stamp) = 2020
+GROUP BY user_id;
